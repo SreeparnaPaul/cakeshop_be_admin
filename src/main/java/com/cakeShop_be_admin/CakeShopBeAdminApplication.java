5b245449d76ef02b52b2e672e5341f7446bce834
SreeparnaPaul <sreeparnapaul21@gmail.com>
2022-07-16 11:01:08 +0530

    first commit

---
+++ src/main/java/com/cakeShop_be_admin/CakeShopBeAdminApplication.java
@@ -0,0 +1,13 @@
+package com.cakeShop_be_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CakeShopBeAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CakeShopBeAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cakeShop_be_admin/CakeShopBeAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cakeShop_be_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CakeShopBeAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
