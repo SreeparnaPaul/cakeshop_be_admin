b59eaef7ee61bd422dc662df5e5e9994d56cd5ef
krishnaSarkar5 <krishnasarkar95@gmail.com>
2022-07-23 22:50:55 +0530

    cors origin problem resolved

--- src/main/java/com/cakeShop_be_admin/controller/CategoryController.java
+++ src/main/java/com/cakeShop_be_admin/controller/CategoryController.java
@@ -3,7 +3,7 @@ package com.cakeShop_be_admin.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,6 +34,7 @@ public class CategoryController {
 		return categoryService.getAllCategory();
 	}
 	
+	@CrossOrigin
 	@PostMapping("/addCategory")
 	public Response addCategory(@RequestBody CategoryRequestDto categoryRequestDto) {
 		return categoryService.addCategory(categoryRequestDto);
